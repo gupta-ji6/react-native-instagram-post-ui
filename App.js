a7887f5752dd5925d020eddfaf8bb284a7840f66
Ayush Gupta <ayush.gupta@razorpay.com>
2024-01-09 22:35:21 +0530

    feat: add single & double dap detection

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { View } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {
+  GestureHandlerRootView,
+  Gesture,
+  GestureDetector,
+} from 'react-native-gesture-handler';
 import { Header } from './components/Header';
 
 export default function InstagramPost(props) {
+  const singleTap = Gesture.Tap()
+    .maxDuration(250)
+    .onStart(() => {
+      console.log('Single tap!');
+    });
+
+  const doubleTap = Gesture.Tap()
+    .maxDuration(250)
+    .numberOfTaps(2)
+    .onStart(() => {
+      console.log('Double tap!');
+    });
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View
@@ -13,6 +30,17 @@ export default function InstagramPost(props) {
         }}
       >
         <Header />
+        <GestureDetector gesture={Gesture.Exclusive(doubleTap, singleTap)}>
+          <View
+            style={{
+              height: 120,
+              width: 120,
+              backgroundColor: '#b58df1',
+              borderRadius: 20,
+              marginBottom: 30,
+            }}
+          />
+        </GestureDetector>
       </View>
     </GestureHandlerRootView>
   );
