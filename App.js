060fb32b7dc94b9ee03a79875600dd886b0ddc20
Ayush Gupta <ayush.gupta@razorpay.com>
2024-01-09 22:26:08 +0530

    build: add rngh

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function InstagramPost(props) {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text>Post Component</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text>Post Component</Text>
+      </View>
+    </GestureHandlerRootView>
   );
 }
