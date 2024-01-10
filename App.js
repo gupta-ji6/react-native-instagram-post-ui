34316d08d078ede3690a05276db42ed7c0918072
ayush gupta <ayush.gupta@razorpay.com>
2024-01-10 14:53:15 +0530

    feat: add double tap to like (#1)
    * chore: clean app.js
    
    * build: add rngh
    
    * feat: add header component
    
    * feat: add single & double dap detection
    
    * feat: add image component
    
    * feat: add old version of double tap to like
    
    * feat: add new double tap like with rotation & tranlsateY
    
    * fix: reset translation after completion
    
    * refactor: add constant for initial values
    
    * refactor: use static assets instead of links

--- App.js
+++ App.js
@@ -1,46 +1,131 @@
+import React, { useCallback } from 'react';
+import {
+  Image,
+  ImageBackground,
+  View,
+  useWindowDimensions,
+} from 'react-native';
+import {
+  GestureHandlerRootView,
+  Gesture,
+  GestureDetector,
+} from 'react-native-gesture-handler';
+import { Header } from './components/Header';
 import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
   Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+const heartRotationDegress = [-35, -25, -15, 15, 25, 35];
+const INITIAL_SCALE_VALUE = 0;
+const INITIAL_ROTATE_VALUE = 0;
+const INITIAL_TRANSLATE_Y_VALUE = 0;
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
+export default function InstagramPost(props) {
+  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+  const scale = useSharedValue(INITIAL_SCALE_VALUE);
+  const rotate = useSharedValue(INITIAL_ROTATE_VALUE);
+  const translateY = useSharedValue(INITIAL_TRANSLATE_Y_VALUE);
 
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
+  const heartStyles = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: Math.max(scale.value, INITIAL_SCALE_VALUE), // added Math.max to avoid negative values when withSping(0) is called
+      },
+      {
+        rotate: `${rotate.value}deg`,
+      },
+      {
+        translateY: translateY.value,
+      },
+    ],
+  }));
+
+  const onDoubelTapStart = useCallback(() => {
+    const randomHeartRotationDegress =
+      heartRotationDegress[
+        Math.floor(Math.random() * heartRotationDegress.length)
+      ];
+
+    scale.value = withSpring(1.5, undefined, (isFinished) => {
+      if (isFinished) {
+        rotate.value = withTiming(
+          randomHeartRotationDegress,
+          undefined,
+          (isFinished) => {
+            if (isFinished) {
+              rotate.value = withTiming(INITIAL_ROTATE_VALUE);
+            }
+          }
+        );
+        translateY.value = withTiming(
+          -windowHeight / 2,
+          {
+            duration: 1250,
+            easing: Easing.inOut(Easing.cubic),
+          },
+          (isFinished) => {
+            if (isFinished) {
+              scale.value = INITIAL_SCALE_VALUE;
+              translateY.value = INITIAL_TRANSLATE_Y_VALUE;
+            }
+          }
+        );
+      }
+    });
+  }, []);
+
+  const singleTap = Gesture.Tap()
+    .maxDuration(250)
+    .onStart(() => {
+      console.log('Single tap!');
+    });
+
+  const doubleTap = Gesture.Tap()
+    .maxDuration(250)
+    .numberOfTaps(2)
+    .onStart(onDoubelTapStart);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
         }}
-      />
-    </View>
+      >
+        <Header />
+        <GestureDetector gesture={Gesture.Exclusive(doubleTap, singleTap)}>
+          <ImageBackground
+            source={require('./assets/dogs.jpeg')}
+            style={{
+              width: windowWidth,
+              height: windowWidth,
+              backgroundColor: 'transparent',
+            }}
+          >
+            <AnimatedImage
+              source={require('./assets/heart.png')}
+              style={[
+                {
+                  width: windowWidth,
+                  height: windowWidth,
+                  shadowOffset: { width: 0, height: 20 },
+                  shadowOpacity: 0.35,
+                  shadowRadius: 35,
+                  backgroundColor: 'transparent',
+                },
+                heartStyles,
+              ]}
+              resizeMode='center'
+            />
+          </ImageBackground>
+        </GestureDetector>
+      </View>
+    </GestureHandlerRootView>
   );
 }
